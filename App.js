e3fe43cea9af284842c62760f3fcf8290c280133
themba <thembamaluleke6@gmail.com>
2025-03-10 14:13:07 +0200

    Deployment checkin

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+   <div className="App">
+    <h1>Stock Market Dashboard</h1>
+    <Dashboard />
+  </div>  
+  );
+}
+
+export default App;
